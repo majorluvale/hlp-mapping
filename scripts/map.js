8fdb39c06b0bdf5b01bcd7db9f193cd00ba06c9b
majorluvale <majorluvale2012@gmail.com>
2023-09-08 14:58:41 +0000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -566,11 +566,12 @@ $(window).on('load', function() {
 
     var countryName = '<h3>' + feature.properties.adm0_name + '</h3>';
     var coordMechanism = '<p class="coordMecha">' + feature.properties.coordMecha + '</p>';
-    var chairCochair = '<p>Chair & co-Chair : ' + '<strong>' + [feature.properties.Chair, feature.properties.coChair].join(', ') + '</strong></p>';
+    var chair = '<p>Chair & co-Chair : ' + '<strong>' + feature.properties.Chair + '</strong></p>';
+    var coChair = '<p>co-Chair : ' + '<strong>' + feature.properties.coChair + '</strong></p>';
     var peopleInNeed = '<p><strong>' + feature.properties.pinF + '</strong> People in need of HLP assistance in 2023</p>';
     var peopleTargeted = '<p><strong>' + feature.properties.targetF + '</strong> People Targeted</p>';
     var fundingRequirement = '<p><strong>' + feature.properties.freqF + '</strong> Funds requested</p>';
-    var info = countryName + coordMechanism + chairCochair + peopleInNeed + peopleTargeted + fundingRequirement + '<p>';
+    var info = countryName + coordMechanism + chair + coChair + peopleInNeed + peopleTargeted + fundingRequirement + '<p>';
 
     var info = '<p>';
     props = allPopupProperties[polygon];
